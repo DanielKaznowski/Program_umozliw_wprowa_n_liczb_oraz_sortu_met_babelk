8e6dc1abe4b335856275d906ae89a6ec0e60d5bc
Daniel_Kaznowski <Daniel_Kaznowski@MR_ROBOT>
2024-11-13 00:14:53 +0100

    Dodaj pliki projektów.

---
+++ Program_umozliw_wprowa_n_liczb_oraz_sortu_met_babelk/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Podaj ilość liczb do posortowania: ");
+        int n = int.Parse(Console.ReadLine());
+
+        int[] liczby = new int[n];
+        Console.WriteLine("Wprowadź liczby:");
+        for (int i = 0; i < n; i++)
+        {
+            Console.Write($"Liczba {i + 1}: ");
+            liczby[i] = int.Parse(Console.ReadLine());
+        }
+
+        // Sortowanie bąbelkowe
+        for (int i = 0; i < liczby.Length - 1; i++)
+        {
+            for (int j = 0; j < liczby.Length - 1 - i; j++)
+            {
+                if (liczby[j] > liczby[j + 1])
+                {
+                    // Zamiana miejscami
+                    int temp = liczby[j];
+                    liczby[j] = liczby[j + 1];
+                    liczby[j + 1] = temp;
+                }
+            }
+        }
+
+        // Wyświetlanie posortowanych liczb
+        Console.WriteLine("Posortowane liczby (sortowanie bąbelkowe):");
+        foreach (var liczba in liczby)
+        {
+            Console.Write(liczba + " ");
+        }
+    }
+}
